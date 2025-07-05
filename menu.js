1350097f0ae8a72980917cbe4bc161d3ede80328
vsostaric <vladimirsostaric3390@gmail.com>
2025-07-05 17:44:20 +0200

    add menu functionality

--- menu.js
+++ menu.js
@@ -2,8 +2,28 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.querySelector('.menu-btn');
     const hamburgerMenu = document.querySelector('.hamburger-menu');
 
-    menuBtn.addEventListener('click', () => {
-        hamburgerMenu.classList.toggle('visible');
-        hamburgerMenu.classList.toggle('hidden');
+    menuBtn.addEventListener('mouseenter', () => {
+        hamburgerMenu.classList.add('visible');
+        hamburgerMenu.classList.remove('hidden');
     });
+
+    menuBtn.addEventListener('mouseleave', () => {
+        setTimeout(() => {
+            if (!hamburgerMenu.matches(':hover')) {
+                hamburgerMenu.classList.remove('visible');
+                hamburgerMenu.classList.add('hidden');
+            }
+        }, 200);
+    });
+
+    hamburgerMenu.addEventListener('mouseleave', () => {
+        hamburgerMenu.classList.remove('visible');
+        hamburgerMenu.classList.add('hidden');
+    });
+
+    hamburgerMenu.addEventListener('mouseenter', () => {
+        hamburgerMenu.classList.add('visible');
+        hamburgerMenu.classList.remove('hidden');
+    });
+
 });
